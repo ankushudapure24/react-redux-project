d3c0cc4b18017f8bdb192f786bfff056144875fc
ankushudapure24 <ankush.udapure@kaliba.in>
2025-02-05 18:49:06 +0530

    Commit at end of day 5 Feb
    Added counter component,
    Added dispatch action for increment and decrement counter.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispaatch = useDispatch();
+  const counter = useSelector(state => state.counter);
 
+  const toggleCounterHandler = () => {
+    // dispatch({ action.type: 'increment });
+  };
+
+  const incrementHandler = () => {
+    dispaatch({ type: 'increment' });
+  }
+
+  const decrementHandler = () => {
+    dispaatch({ type: 'decrement' });
+  }
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={decrementHandler}>Decrement</button>  
+        <button onClick={incrementHandler}>Increment</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
